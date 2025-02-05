1e1d764966f9a1c7a3a7ee188ea31c4af058199e
steinhauer <jg225260@dal.ca>
2025-02-05 16:03:08 -0400

    commit of all create account files UI

---
+++ app/src/main/java/com/example/quickcash/ui/CreateAccount.java
@@ -0,0 +1,14 @@
+package com.example.quickcash.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quickcash.R;
+
+public class CreateAccount extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
